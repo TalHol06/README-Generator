0d2116f36501056a737229e903fb9ca70064a4f3
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2024-11-14 17:31:09 -0600

    added code for all JavaScript files

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+// const { error } = require('console');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description about your project.'
+  },
+  {
+    type: 'input',
+    name: 'installing',
+    message: 'Enter the installation steps for your project, if any.'
+  }, 
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for the use of your project.'
+  }, 
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select one of the following licenses.',
+    choices: ["MIT", "Apache 2.0", "MPL 2.0", "ISC", "BSD", "Boost", "None"]
+  }, 
+  {
+    type: 'input',
+    name: 'contrbutors',
+    message: 'Enter all who contributed to this project.'
+  }, 
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter any test that you did for your application.'
+  }, 
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username.'
+  }, 
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address.'
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // Prompt the User to input title, description, contributors, etc.
+  fs.writeFile(fileName, data, (err) =>{
+    if (err){
+      console.log('Error occured while creating README file', err);
+    }else{
+      console.log('README file created successfully!');
+    }
+    
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(answers =>{
+    console.log('Generating your README file...');
+    writeToFile("professionalREADME.md", generateMarkdown(answers));
+  }).catch(error => {
+    console.log('Error:', error);
+  })
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None"){
+    return "";
+  }
+
+  return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None"){
+    return "";
+  }
+  return `[License](https://opensource.org/licenses/${license})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None"){
+    return "";
+  }
+  return `## License`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Contributors](#contributors)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installing}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.github}\n
+  ${data.email}
+
+  ## License
+  ${data.license}${renderLicenseLink(data.license)}
 `;
 }
 
-export default generateMarkdown;
+module.exports = generateMarkdown;
